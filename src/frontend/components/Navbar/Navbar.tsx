6b49b41d731aef48755d8ccea5c49e9606a322b9
Mitch281 <mitchella0100@gmail.com>
2024-01-24 23:32:17 +1100

    Logout button functionality.

--- src/frontend/components/Navbar/Navbar.tsx
+++ src/frontend/components/Navbar/Navbar.tsx
@@ -2,19 +2,31 @@
 
 import { Button } from "@mui/material";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
     const [userId, setUserId] = useState<string | null>("");
+    const router = useRouter();
 
     useEffect(() => {
         setUserId(localStorage.getItem("userId"));
     }, []);
 
+    function logout() {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("isAdmin");
+        router.push("/login");
+    }
+
     let navbarButtonElements = <></>;
     if (userId) {
-        navbarButtonElements = <Button variant="contained">Logout</Button>;
+        navbarButtonElements = (
+            <Button variant="contained" onClick={logout}>
+                Logout
+            </Button>
+        );
     } else {
         navbarButtonElements = (
             <>
