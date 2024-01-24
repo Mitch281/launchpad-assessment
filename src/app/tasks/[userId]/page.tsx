b170865e01b559deffbf30548af540705778c53f
Mitch281 <mitchella0100@gmail.com>
2024-01-25 01:09:33 +1100

    You have no tasks message.

--- src/app/tasks/[userId]/page.tsx
+++ src/app/tasks/[userId]/page.tsx
@@ -1,9 +1,21 @@
 import fetchAllTasksByUser from "@/frontend/api/fetchAllTasksByUser";
+import PageLayout from "@/frontend/components/PageLayout/PageLayout";
 import Tasks from "@/frontend/components/Tasks/Tasks";
 import { Task } from "@prisma/client";
 
 export default async function Page({ params }: { params: { userId: string } }) {
     const response = await fetchAllTasksByUser(params.userId);
-    const allTasks = response.data;
-    return <Tasks allTasks={allTasks as Task[]} />;
+    const allTasks = response.data as Task[];
+    if (allTasks.length === 0) {
+        return (
+            <PageLayout>
+                <span>You have no Tasks!</span>
+            </PageLayout>
+        );
+    }
+    return (
+        <PageLayout>
+            <Tasks allTasks={allTasks as Task[]} />
+        </PageLayout>
+    );
 }
