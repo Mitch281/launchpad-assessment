5fddf90eb39a78ed084aab39bec26f12651f5501
Mitch281 <mitchella0100@gmail.com>
2024-01-25 01:06:28 +1100

    Fix caching issues.

--- src/frontend/api/fetchAllTasksByUser.ts
+++ src/frontend/api/fetchAllTasksByUser.ts
@@ -3,7 +3,9 @@ import { FetchTasksResponse } from "@/shared/types";
 const API_URL = process.env.API_URL;
 
 export default async function fetchAllTasksByUser(userId: string) {
-    const response = await fetch(`${API_URL}/tasks?userId=${userId}`);
+    const response = await fetch(`${API_URL}/tasks?userId=${userId}`, {
+        cache: "no-store",
+    });
     const json: FetchTasksResponse = await response.json();
     if (!response.ok) {
         throw new Error(json.message);
--- src/frontend/api/fetchUsers.ts
+++ src/frontend/api/fetchUsers.ts
@@ -1,7 +1,9 @@
 const API_URL = process.env.API_URL;
 
 export default async function fetchUsers() {
-    const response = await fetch(`${API_URL}/users`);
+    const response = await fetch(`${API_URL}/users`, {
+        cache: "no-store",
+    });
     const json = await response.json();
     if (!response.ok) {
         throw new Error(json.message);
